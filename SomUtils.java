84d863bb261369a3d8b5b3843656f0e46bddcdbb
Samuel <samuel.gfries@gmail.com>
2024-06-13 10:30:18 -0300

    .

---
+++ SomUtils.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class SomUtils {
+    public static void tocarSom(String caminhoDoSom) {
+        try {
+            File arquivoDeSom = new File(caminhoDoSom);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(arquivoDeSom);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+}
