43890b80e2eb58d88563a0658fdf9bd11449f110
Fries <84678500+SamuelFries@users.noreply.github.com>
2024-06-24 19:32:20 -0300

    Add files via upload
    Signed-off-by: Fries <84678500+SamuelFries@users.noreply.github.com>

---
+++ PinoColorido.java
@@ -0,0 +1,25 @@
+
+import java.awt.event.ActionEvent;
+
+public class PinoColorido extends Pino {
+
+    protected PinoColorido(Cor cor) {
+        super(cor);
+    }
+
+    public static PinoColorido criaPinoColorido(String nomeCor) {
+        Cor cor = Cores.getInstance().getCor(nomeCor);
+        if (cor == null) {
+            throw new IllegalArgumentException("Cor invalida: " + nomeCor);
+        }
+        return new PinoColorido(cor);
+    }
+
+    @Override
+    public void acaoDoBotao(ActionEvent e) {
+        Cor novaCor = Cores.getInstance().proximaCor(getCor().getNomeCor());
+        setCor(novaCor);
+        Som.tocarSom(); // Chama o método tocarSom() para tocar o som definido na classe Som
+    }
+}
+    
---
+++ PinoPB.java
@@ -0,0 +1,40 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+
+public class PinoPB extends Pino {
+    private PinoPB(Cor cor){
+        super(cor);
+    }
+
+    public static PinoPB criaPinoPreto(){
+        return new PinoPB(new Cor("BLACK", Color.BLACK));
+    }
+
+    public static PinoPB criaPinoBranco(){
+        return new PinoPB(new Cor("WHITE", Color.WHITE));
+    }
+
+    public static PinoPB criaPinoCinza(){
+        return new PinoPB(new Cor("GRAY", Color.GRAY));
+    }
+
+    public static PinoPB criaPinoVazio(){
+        return new PinoPB(new Cor("EMPTY", Color.WHITE));
+    }
+
+    public void setCor(Cor cor){
+        if (cor.getNomeCor().equals("BLACK") ||
+            cor.getNomeCor().equals("WHITE") ||
+            cor.getNomeCor().equals("GRAY") ||
+            cor.getNomeCor().equals("EMPTY")) {
+            super.setCor(cor);    
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    @Override
+    public void acaoDoBotao(ActionEvent e) {
+        // Em princípio os pinos de pista não têm ação sobre o jogo quando acionados
+    }
+}
